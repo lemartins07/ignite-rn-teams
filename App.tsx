b92377af98f0a34cc69b0f3d661764f81feed7e9
Leandro Martins <leandro.martins.85@gmail.com>
2023-12-26 15:50:27 -0300

    Estilizando StatusBar

--- App.tsx
+++ App.tsx
@@ -6,13 +6,18 @@ import {
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, StatusBar } from 'react-native';
 import theme from './src/theme';
 
 export default function App(): React.JSX.Element {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
