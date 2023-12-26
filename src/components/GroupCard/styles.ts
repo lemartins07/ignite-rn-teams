c465498d9ecd9f819658b0afeeb424392e8acffb
Leandro Martins <leandro.martins.85@gmail.com>
2023-12-26 18:04:29 -0300

    Componente GroupCard

---
+++ src/components/GroupCard/styles.ts
@@ -0,0 +1,32 @@
+import { UsersThree } from 'phosphor-react-native';
+import { TouchableOpacity } from 'react-native';
+import { useTheme } from 'styled-components';
+import styled from 'styled-components/native';
+
+export const Container = styled(TouchableOpacity)`
+  width: 100%;
+  height: 90px;
+
+  background-color: ${() => useTheme().COLORS.GRAY_500};
+  border-radius: 6px;
+
+  flex-direction: row;
+  align-items: center;
+
+  padding: 24px;
+  margin-bottom: 12px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${() => useTheme().FONT_SIZE.MD}px;
+  color: ${() => useTheme().COLORS.GRAY_200};
+  font-family: ${() => useTheme().FONT_FAMILY.REGULAR};
+`;
+
+export const Icon = styled(UsersThree).attrs(() => ({
+  size: 32,
+  color: useTheme().COLORS.GREEN_700,
+  weight: 'fill',
+}))`
+  margin-right: 20px;
+`;
